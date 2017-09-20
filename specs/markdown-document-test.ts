bcdaf0858722f4ce0ad48aed9d5f962ce5bea61f
Marcel Hofer <the.hoferm@gmail.com>
2017-09-20 18:24:40 +0200

    Add MarkdownDocument with simple test

---
+++ specs/markdown-document-test.ts
@@ -0,0 +1,15 @@
+/// <reference path="include.d.ts" />
+
+import * as should from "should";
+
+import { IOptions, MarkdownDocument } from "../src/markdown-document";
+
+describe('MarkdownDocument', function() {
+    it('constructor throws no exception', function() {
+        // Arrange
+        const options = <IOptions>{ document: 'example.md' };
+
+        // Act
+        const document = new MarkdownDocument(options);
+    });
+});
---
+++ src/markdown-document.ts
@@ -0,0 +1,8 @@
+export interface IOptions {
+    document: string;
+}
+
+export class MarkdownDocument {
+    constructor(private options: IOptions) {
+    }
+}
