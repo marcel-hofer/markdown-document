662d7f903c9cd97aaf74c5410aebfcd94fd6e5ab
Marcel Hofer <the.hoferm@gmail.com>
2017-10-10 19:46:25 +0200

    Implement setMetadata

--- src/definitions/node-exiftool.d.ts
+++ src/definitions/node-exiftool.d.ts
@@ -11,13 +11,18 @@ declare module "node-exiftool" {
         open(): q.IPromise<number>;
         close(): q.IPromise<void>;
 
-        readMetadata<TMetadata>(file: string, arguments?: string[]): q.IPromise<TMetadata | IDefaultMetadata>;
-        writeMetadata(file: string, data: any, args: string[]): q.IPromise<void>;
+        readMetadata<TMetadata extends IDefaultMetadata>(file: string, arguments?: string[]): q.IPromise<IReadMetadataResult<TMetadata>>;
+        writeMetadata(file: string, data: any, args?: string[]): q.IPromise<void>;
     }
 
     export interface IDefaultMetadata {
-        SourceFile: string;
-        ExifToolVersion: string;
+        SourceFile?: string;
+        ExifToolVersion?: string;
+    }
+
+    export interface IReadMetadataResult<TMetadata extends IDefaultMetadata> {
+        data: TMetadata[];
+        error: any;
     }
 
     export default INodeExiftool;
